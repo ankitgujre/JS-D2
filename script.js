4d35af53de26b524e0f575d401636382857649f7
ankitgujre <ankitgujre374@gmail.com>
2025-10-26 18:57:00 +0530

    template string split slice replace includes

--- script.js
+++ script.js
@@ -16,13 +16,14 @@
 
 // console.log(c)   // console me print karwata hai
 
-console.info("Hello world")   //kuch kuch browser me i sign aajata hai
+// console.info("Hello world")   //kuch kuch browser me i sign aajata hai
 
-console.error("eroor dikhane ke liye")
+// console.error("eroor dikhane ke liye")
 
-console.warn("Warning dikhane ke liye")
+// console.warn("Warning dikhane ke liye")
 
 // console.table({"Name":"Ankit"})
+/*
 console.table({Name:"Ankit"})
 
 let nm = prompt("what is your name")
@@ -33,4 +34,17 @@ console.log(nm)
 document.writeln("this is string")
 
 let intro = "I am ankit"
-console.log(intro.slice(4,6))
+console.log(intro.slice(4,6))*/
+
+
+// `template string`
+
+// console.log(`Ankit ${13+10}`)
+
+// console.log("ankit".split(''))
+// console.log("Hello bhai kese ho".split('h'))
+console.log("Hello bhai kese ho".replace('h','g'))  // h ko replace kardiya g se lekin sirf pehle vala
+console.log("Hello bhai kese ho".replaceAll('h','g')) // sare ko change karne ke liye
+
+console.log("sdkdjfdkdfjn".includes("h"))  // h exists krta hai ya nahi
+
