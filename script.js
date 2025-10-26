8b8ea505eb425eddaa1134b05101ccad9ed8d949
ankitgujre <ankitgujre374@gmail.com>
2025-10-26 15:30:57 +0530

    Console.log and info

---
+++ script.js
@@ -0,0 +1,19 @@
+// var let const
+
+// var a = 12;
+// let a = 13;
+
+{
+    var b = 45;   // ye print kar raha hai
+}
+
+console.log(b)  
+
+{
+    let c = 65;   /*script.js:16 Uncaught ReferenceError: c is not defined
+    at script.js:16:13 yeh error dega*/
+}
+
+console.log(c)   // console me print karwata hai
+
+// console.info("Hello world")   kuch kuch browser me i sign aajata hai
